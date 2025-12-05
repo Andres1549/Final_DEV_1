33ba43f8adfaf1a56668b9d7847c7e28d38b8ef9
sala4 <japarra68@ucatolica.edu.co>
2025-12-05 08:17:22 -0500

    requerimientos y enum de pie dominante y jugador modelo

--- models.py
+++ models.py
@@ -1,5 +1,20 @@
+from utils import *
+
+from typing import Optional
+from sqlmodel import SQLModel, Field
+
 class Jugador():
-    pass
+    id: Optional[int] = Field(default=None, primary_key=True)
+    nombre: str
+    n_camiseta: int 
+    año_nacimiento:int
+    nacionalidad:str
+    altura:int
+    peso:int
+    pie_dom: Pie
+    posicion:Position
+    año_ingreso:int
+    estado: States
 
 
 class Estadistica():
