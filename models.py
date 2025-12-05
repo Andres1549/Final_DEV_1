1eae22c0c7564286380f71dcc1ddf25cc4c5aeef
sala4 <afbasto49@ucatolica.edu.co>
2025-12-05 08:35:16 -0500

    enum bien importados

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
-from utils import *
-
 from typing import Optional
 from sqlmodel import SQLModel, Field
+from utils.dominante import Pie
+from utils.positions import Position
+from utils.states import States
 
 class Jugador():
     id: Optional[int] = Field(default=None, primary_key=True)
