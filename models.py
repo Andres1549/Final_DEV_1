dfa261abc4585cb637f5bd844f3386d2d0b8292e
sala4 <afbasto49@ucatolica.edu.co>
2025-12-05 08:56:22 -0500

    estadisticas partido no se si esta bien

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
-from typing import Optional
-from sqlmodel import SQLModel, Field
+from typing import Optional,List
+from sqlmodel import SQLModel, Field, Relationship
 from utils.dominante import Pie
 from utils.positions import Position
 from utils.states import States
@@ -7,7 +7,7 @@ from utils.states import States
 class Jugador():
     id: Optional[int] = Field(default=None, primary_key=True)
     nombre: str
-    n_camiseta: int 
+    n_camiseta: int #1 a 99 no se puede repetir
     año_nacimiento:int
     nacionalidad:str
     altura:int
@@ -18,8 +18,13 @@ class Jugador():
     estado: States
 
 
-class Estadistica():
-    pass
+class Estadistica_partido():
+    id: Optional[int] = Field(default=None, primary_key=True)
+    id_jugador: list["Jugador"] = Relationship(back_populates="jugador")
+    id_partido: list["Partido"] = Relationship(back_populates="partido")
+    tarjetas:int #no mayor a 2, es expulsion
+    min_jugados:int
+#debemos hacer estadisticas generales con esta tabla 
 
 
 class Partido():
